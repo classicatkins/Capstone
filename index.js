12f7cecb9137e5a2a1be491c9deaade4bf06b594
Melissa Atkins <classicatkins@gmail.com>
2023-12-01 23:10:47 -0600

    stats

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('.nav-link');
+  const currentPage = window.location.pathname;
+
+  links.forEach(link => {
+      if (link.getAttribute('href') === currentPage) {
+          link.classList.add('active-link');
+      }
+  });
+});
